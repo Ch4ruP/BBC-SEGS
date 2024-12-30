92baf41d90b15d85ffc3c5d983244fc8c058d7ff
Charu <cxp111@student.bham.ac.uk>
2024-12-30 11:04:39 +0000

    add 52 cards to deck

--- src/deck.py
+++ src/deck.py
@@ -1,3 +1,4 @@
 class Deck:
     def __init__(self):
-        self.cards = []
+        self.cards = ['A',2,3,4,5,6,7,8,9,10,'J','Q','K']*4
+        
--- test/test_deck.py
+++ test/test_deck.py
@@ -13,7 +13,13 @@ class DeckTestCase(unittest.TestCase):
     def test_number_of_cards(self):  # any method beginning with 'test_' will be run by unittest
         number_of_cards = len(self.deck.cards)
         self.assertEqual(number_of_cards, 52)
-
+    def test_card_values(self):
+        number_of_aces = self.deck.cards.count("A")
+        number_of_sevens = self.deck.cards.count(7)
+        number_of_kings = self.deck.cards.count("K")
+        self.assertEqual(number_of_aces, 4)
+        self.assertEqual(number_of_sevens, 4)
+        self.assertEqual(number_of_kings, 4)
 
 if __name__ == '__main__':
     unittest.main()
